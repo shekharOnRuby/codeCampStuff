2345bf80f412605ed65c935808b5209730eaf0c8
shekhar karande <shekhar.karande@gmail.com>
2020-06-14 21:33:39 +0100

    React+Express:
    Finishing touches

--- react_node_sample/client/src/App.js
+++ react_node_sample/client/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learning React - June 2020
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+      fetch("http://localhost:9000/testAPI")
+          .then(response => response.text())
+          .then(response => this.setState({ apiResponse: response }));
+  }
+
+  componentDidMount() {
+      this.callAPI();
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learning React - June 2020
+          </a>
+        </header>
+        <p className='App-intro'>Response -> {this.state.apiResponse}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
